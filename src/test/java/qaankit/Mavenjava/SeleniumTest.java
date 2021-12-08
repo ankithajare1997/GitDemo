6882ed90af2bac17ce045d9324e2ebd9584fc0ad
Ankit Hajare <ankithajare1997@gmail.com>
2021-12-08 11:09:06 +0530

    first commit

---
+++ src/test/java/qaankit/Mavenjava/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaankit.Mavenjava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+
+	@Test
+	public void elementUI() {
+		System.out.println("elementUI");
+	}
+}
